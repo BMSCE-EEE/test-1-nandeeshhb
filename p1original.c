caabde870d0411c809fcbf8dca9b182ffe80fed4
nandeeshhb <94944778+nandeeshhb@users.noreply.github.com>
2021-11-29 03:18:50 +0000

    abc

--- p1original.c
+++ p1original.c
@@ -1,2 +1,18 @@
-
+#include<stdio.h>
+int input();
+int main()
+{
+  int num1,num2,sum;
+  num1=input();
+  num2=input();
+  sum=num1+num2;
+  printf("Sum of two numbers is:%d",sum);
+  return 0;
+}
+int input()
+{
+int A;
+printf("Enter a number:\n");
+scanf("%d",&A);
+return A;
 }
